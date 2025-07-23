98338fec57adc9e5afd9b0ea713759b043c42ad6
PolarisWater <janpawlak.pl@gmail.com>
2025-07-23 22:22:03 +0200

    version 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="api-hijacker",
-    version="0.1.1",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "requests>=2.9.2",
