019f679974ced3e3cc348cdf29d86902f4d0923e
PolarisWater <janpawlak.pl@gmail.com>
2025-07-23 21:14:49 +0200

    changed naming

--- apiCore/exceptions.py
+++ apiCore/exceptions.py
@@ -1,18 +1,18 @@
-class ApiCoreError(Exception):
+class ApiCoreException(Exception):
     def __init__(self, error):
         super().__init__(error)
 
 
-class TerminalStatusCodeError(ApiCoreError):
+class TerminalStatusCode(ApiCoreException):
     def __init__(self, statusCode: int, functionName: str):
         super().__init__(f"function {functionName} got a terminal status code - HTTP {statusCode}")
 
 
-class ExceptionInRequestFunctionError(ApiCoreError):
+class ExceptionInRequestFunction(ApiCoreException):
     def __init__(self, functionName: str):
         super().__init__(f"function {functionName}: Exception occurred")
 
 
-class AllRetriesFailedError(ApiCoreError):
+class AllRetriesFailed(ApiCoreException):
     def __init__(self, retries: int, lastStatusCode: int, functionName: str):
         super().__init__(f"function {functionName}: all {retries} retries failed   last status code - HTTP {lastStatusCode}")
